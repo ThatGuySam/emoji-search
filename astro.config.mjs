c2ac5dc0febe6929a0604ef77892a8b6d9aa3887
ThatGuySam <sam@sam.lc>
2025-08-14 23:50:42 -0500

    update: setup cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
   vite: {
     optimizeDeps: {
       exclude: ['@electric-sql/pglite']
     }
-  }
+  },
+
+  adapter: cloudflare()
 });
