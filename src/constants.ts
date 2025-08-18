69de77a911c46bec225faea848a4fdcef86b8b4b
ThatGuySam <sam@sam.lc>
2025-08-17 23:26:52 -0500

    update: store tar dbs in src dir

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,6 @@ export const MODELS_PATH_TEMPLATE = '{model}/resolve/{revision}/'
 // https://huggingface.co/Supabase/gte-small/tree/main
 export const SUPA_GTE_SMALL = 'supabase/gte-small'
 
-export const OUT_DIR = './public'
+export const OUT_DIR = './src/artifacts'
 export const DB_TAR = `${OUT_DIR}/emoji.tar`
 export const DB_TAR_BR = `${DB_TAR}.br`
